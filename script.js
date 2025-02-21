33cd77b651b787af11ae64e1e8c8d59ae1eb8e8d
DucklingVivi <95558019+DucklingVivi@users.noreply.github.com>
2025-02-21 18:57:46 -0500

    werewr

---
+++ script.js
@@ -0,0 +1,34 @@
+adapter = import('./adapter-latest.js');
+
+
+
+const displayMediaOptions = {
+    video: true,
+    audio: false
+  };
+  
+  
+async function startCapture(displayMediaOptions) {
+    let captureStream = null;
+    console.log(adapter)
+    try {
+      captureStream =
+        await adapter.browserShim.shimGetDisplayMedia(displayMediaOptions);
+    } catch (err) {
+      console.error(`Error: ${err}`);
+    }
+    return captureStream;
+  }
+
+function start(){
+    startCapture(displayMediaOptions).then((stream) => {
+
+        videoObj = document.getElementById("video");
+        
+        //unhide video 
+
+        videoObj.style.display = "block";
+
+        videoObj.srcObject = stream;
+    });
+}
